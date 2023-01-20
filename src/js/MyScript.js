5354f61d0c6116a2d611f85033409293227b0ffb
Aleksey02ry <116436694+Aleksey02ry@users.noreply.github.com>
2023-01-20 14:17:49 +0300

    Update

--- src/js/MyScript.js
+++ src/js/MyScript.js
@@ -1,88 +1,31 @@
-
-/*let sum = 0, days = 0;
-function calc(){
-let site = prompt("Какой тип сайт ты хочешь? 1 - Визитка, 2 - Корпоративный сайт, 3 - Интернет-мазагин");
+$(document).ready(function(){
     
-    if( site == "1"){
-    sum += 800;
-    days += 2;
-}else if( site == "2"){
-    sum+= 3000;
-    days += 10;
-}else {
-    alert("Вы ввели, что-то не верно");
-    calc();
+    function calculate(){
+      let sum = parseInt($("#select1 option:selected").val()) + parseInt($("#select2 option:selected").val()) + parseInt($("#select3 option:selected").val());
+       let days = parseInt($("#select1 option:selected").attr("days")) + parseInt($("#select2 option:selected").attr("days")) + parseInt($("#select3 option:selected").attr("days"));
+       $(".days .cena_c").text(days);
+       $(".price .cena_i").text(sum);
     }
-
-let design = prompt("Какой тип дизайн ты хочешь? 1 - Шаблонный, 2 - Уникальный, 3 - WOW-Дизайн");
+    $("select").on("change", function(){
+        calculate();
+   });
+    calculate();
     
-    if( design == "1"){
-    sum += 500;
-    days += 1;
-}else if( design == "2"){
-    sum+= 1000;
-    days += 1;
-}else {
-    alert("Вы ввели, что-то не верно");
-    calc();
-    }
     
-let adaptive = prompt("Какой тип адаптации тебе нужен? 1 - Только Пк, 2 - Только мобильный сайт, 3 - Пк + Мобильный");
-    
-    if( adaptive == "1"){
-    sum += 500;
-    days += 1;
-}else if( adaptive == "2"){
-    sum+= 500;
-    days += 1;
-}else {
-    alert("Вы ввели, что-то не верно");
-    calc();
-    }
-
-alert("Сроки: " + days + " Стоимость: " + sum);
-}
-calc();*/
-
-let site, design, adaptive;
-
-let calculator = {
-    price: [
-        [100, 500, 1000],
-        [500, 1000, 2000],
-        [1000, 2000, 5000],
-    ],
-    days: [
-        [1, 2, 3],
-        [4, 5, 6],
-        [7, 8, 9],
-    ],
-    run(siteType, desingType, adaptiveType){
-        let sum = parseInt(calculator.price[0][siteType-1]) + parseInt(calculator.price[1][desingType-1]) + parseInt(calculator.price[2][adaptiveType-1]);
-        let days = parseInt(calculator.days[0][siteType-1]) + parseInt(calculator.days[1][desingType-1]) + parseInt(calculator.days[2][adaptiveType-1]);
-        alert("Сроки: " + days + " Стоимость: " + sum);
-    }
-}
-function getAnswers() {
-site = prompt("Какой тип сайт ты хочешь? 1 - Визитка, 2 - Корпоративный сайт, 3 - Интернет-мазагин");
-    if(site != 1 && site != 2 && site != 3){
-        alert("Такого варианта нет!");
-        getAnswers();
-        return;
-    }
-design = prompt("Какой тип дизайн ты хочешь? 1 - Шаблонный, 2 - Уникальный, 3 - WOW-Дизайн");
-    if(design != 1 && design != 2 && design != 3){
-        alert("Такого варианта нет!");
-        getAnswers();
-        return;
-    }
-adaptive = prompt("Какой тип адаптации тебе нужен? 1 - Только Пк, 2 - Только мобильный сайт, 3 - Пк + Мобильный");
-    if(adaptive != 1 && adaptive != 2 && adaptive != 3){
-        alert("Такого варианта нет!");
-        getAnswers();
-        return;
-    }
-    calculator.run(site, design, adaptive);
-}
-
-getAnswers();
+    $(window).scroll(() => {
+        let scrollDistance = $(wondow).scrollTor();
+        
+        $('.selection').each((i, el) => {
+            
+            if ($(el).offset().top - $('.nav').outerHeight() <= scrollDistance) {
+                $('.nav').each((i, el) => {
+                    if ($(el).hasClass('active')) {
+                        $(el).removeClass('active');
+                    }
+                });
+                
+                $('nav li:eq(' + i + ')').find('a').addClass('active');
+            }
+        });
+    });
+});
