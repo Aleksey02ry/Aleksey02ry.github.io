cb83727d86444431b282ccb1c775211832ae0859
Aleksey02ry <116436694+Aleksey02ry@users.noreply.github.com>
2023-02-06 17:19:06 +0300

    ОБНОВИЛ

--- src/js/MyScript.js
+++ src/js/MyScript.js
@@ -11,22 +11,28 @@ $(document).ready(function(){
         calculate();
    });
     calculate();
+
+$('a[href^="#"]').click(function(){
+    let valHref = $(this).attr("href");
+    $('html, body').animate({scrollTop: $(valHref).offset().top - 60 + "px"});
+});
     
     
     $(window).scroll(() => {
         let scrollDistance = $(window).scrollTop();
         
-        $('.selection').each((i, el) => {
+        $(".selection").each((i, el) => {
             
-            if ($(el).offset().top - $('.nav').outerHeight() <= scrollDistance) {
-                $('.nav').each((i, el) => {
-                    if ($(el).hasClass('active')) {
-                        $(el).removeClass('active');
+            if($(el).offset().top - $(".nav").outerHeight() <= scrollDistance){
+                $("nav a").each((i, el) => {
+                    if ($(el).hasClass("active")) {
+                        $(el).removeClass("active");
                     }
                 });
                 
-                $('nav li:eq(' + i + ')').find('a').addClass('active');
+                $('nav li:eq('+ i +')').find('a').addClass('active');
             }
         });
     });
 });
+
