f641e8241d019dec2924a9013ed9dc3581228611
Aleksey02ry <116436694+Aleksey02ry@users.noreply.github.com>
2023-02-26 16:31:14 +0300

    Update

--- src/js/MyScript.js
+++ src/js/MyScript.js
@@ -201,3 +201,39 @@ function onEntry (entry){
 $(document).ready(function() {
   $('.image-link').magnificPopup({type:'image'});
 });
+
+$('.count').each(function (){
+    $(this).prop('statistic_item',0).animate({
+        statistic_item: $(this).text()
+    }, {
+        duration: 4000,
+        easing: 'swing',
+        step:function(now){
+            $(this).text(Math.ceil(now));
+        }
+    });
+});
+
+$(document).ready(function () {
+ 
+    var show = true;
+    var countbox = ".statistic_container";
+    $(window).on("scroll load resize", function () {
+        if (!show) return false; // Отменяем показ анимации, если она уже была выполнена
+        var w_top = $(window).scrollTop(); // Количество пикселей на которое была прокручена страница
+        var e_top = $(countbox).offset().top; // Расстояние от блока со счетчиками до верха всего документа
+        var w_height = $(window).height(); // Высота окна браузера
+        var d_height = $(document).height(); // Высота всего документа
+        var e_height = $(countbox).outerHeight(); // Полная высота блока со счетчиками
+        if (w_top + 500 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height) {
+            $('.count').css('opacity', '1');
+            $('.count').spincrement({
+                thousandSeparator: "",
+                duration: 1200
+            });
+             
+            show = false;
+        }
+    });
+ 
+});
